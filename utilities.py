d1fa44d8f839689e0e225dfd60581d019d5b6278
Kurt Trey Trampel <46588078+KurtTrampel@users.noreply.github.com>
2022-04-04 13:26:13 -0400

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -1,1 +1,14 @@
-# utility functions for network creation and training
+'''
+utility functions for network creation and training
+'''
+
+'''
+functions to create:
+
+1) creates a network that serves as a representation of a group generator
+2) creates a multiple of networks for each generator of the group using (1)
+3) train the network on the relations of the group *
+    * confusion about how the group relations are being used for network loss here
+4) gives the error on satisfying the relation of the group
+
+'''
