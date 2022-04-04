db1b3522578f9b86f0857241a5a21c61873b9590
Kurt Trey Trampel <46588078+KurtTrampel@users.noreply.github.com>
2022-04-04 13:34:06 -0400

    Add outline of work to do

--- zxz_group_rep_network.py
+++ zxz_group_rep_network.py
@@ -1,1 +1,9 @@
 # network for training relations for ZxZ
+
+'''
+Outline of functions to create:
+
+1) "auxilarly" network, used to train the other networks that serve as representations of the generators
+2) a loss function for the relations of ZxZ, i.e. commutativity
+
+'''
