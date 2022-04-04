a2ac7aad925f2bd18724dace88c4ab13bdbc4a87
Kurt Trey Trampel <46588078+KurtTrampel@users.noreply.github.com>
2022-04-04 13:16:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-lBlame Trey for this.
+'''
+File to run that prompts choice of parameters.
+'''
+
+'''
+Outline:
+need python agrument parsing
+    might want something that allows for options without having to go through option each time
+    defaults, etc.
+this will run the appropriate file for training or testing the network under the group/parameter choices
+'''
