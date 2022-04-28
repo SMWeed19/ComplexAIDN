58748fe7fb2a736d067dc459a30c80b86e043faf
Kurt Trey Trampel <46588078+KurtTrampel@users.noreply.github.com>
2022-04-28 13:29:59 -0400

    Create SL2_network.py
    Listed the desired functions needed for SL_2.

---
+++ SL2_network.py
@@ -0,0 +1,5 @@
+"""
+1. Create an auxiliary network to train the network representations of the three generators E, F, H on the commutator relations.
+2. Create the loss function for the purpose of training the networks on these commutator relations.
+3. Eventually have the networks over the Complex numbers rather than Reals.
+"""
